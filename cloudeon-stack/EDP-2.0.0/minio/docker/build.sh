9e6957c498500674776da77f9db93bfbb0438e00
yixiao <1318863597@qq.com>
2025-05-08 10:11:06 +0800

    CloudEon集成minio:2021-04-22T15-44-28Z

--- cloudeon-stack/EDP-2.0.0/minio/docker/build.sh
+++ cloudeon-stack/EDP-2.0.0/minio/docker/build.sh
@@ -1,2 +1,2 @@
-docker build -t registry.cn-guangzhou.aliyuncs.com/bigdata200/minio:2021-04-22T15-44-28Z .
-docker push  registry.cn-guangzhou.aliyuncs.com/bigdata200/minio:2021-04-22T15-44-28Z
+docker build -t registry.cn-shenzhen.aliyuncs.com/yixiao_cloudeon/minio:2021-04-22T15-44-28Z .
+docker push  registry.cn-shenzhen.aliyuncs.com/yixiao_cloudeon/minio:2021-04-22T15-44-28Z
