7a6247d1fab258b5275803b168f33bcfed8b40c8
yixiao <1318863597@qq.com>
2025-05-08 10:11:52 +0800

    CloudEon集成升级Seatunnel:2.3.8 && Seatunnel_Web:1.0.2

--- cloudeon-stack/EDP-2.0.0/seatunnel/docker/build.sh
+++ cloudeon-stack/EDP-2.0.0/seatunnel/docker/build.sh
@@ -1,7 +1,7 @@
-docker build  -f Dockerfile -t registry.cn-guangzhou.aliyuncs.com/bigdata200/seatunnel:2.3.7  .
-docker push  registry.cn-guangzhou.aliyuncs.com/bigdata200/seatunnel:2.3.7
+docker build  -f Dockerfile -t registry.cn-shenzhen.aliyuncs.com/yixiao_cloudeon/seatunnel:2.3.8  .
+docker push  registry.cn-shenzhen.aliyuncs.com/yixiao_cloudeon/seatunnel:2.3.8
 
-docker build  -f Dockerfile_web -t registry.cn-guangzhou.aliyuncs.com/bigdata200/seatunnel-web:1.0.1  .
-docker push  registry.cn-guangzhou.aliyuncs.com/bigdata200/seatunnel-web:1.0.1
+docker build  -f Dockerfile_web -t registry.cn-shenzhen.aliyuncs.com/yixiao_cloudeon/seatunnel-web:1.0.2  .
+docker push  registry.cn-shenzhen.aliyuncs.com/yixiao_cloudeon/seatunnel-web:1.0.2
 
 
